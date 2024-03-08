283d5d6fc0df280a8d8980fb2ac805a0d7c344ed
chandrima0503 <chandrimam06@gmail.com>
2024-03-09 05:20:41 +0530

    added more projects

--- script.js
+++ script.js
@@ -11,3 +11,30 @@ setTimeout(() => {
       });
     });
   }, 100);
+
+  function openVideoPlayer() {
+    // Replace 'your-video-url.mp4' with the actual URL or path to your video file
+    var videoUrl = 'ScreenRecord.mp4';
+
+    // Create the HTML content for the video player
+    var videoContent = `
+      <html>
+      <head>
+        <title>Video Player</title>
+      </head>
+      <body>
+        <video width="640" height="360" controls autoplay>
+          <source src="${videoUrl}" type="video/mp4">
+          Your browser does not support the video tag.
+        </video>
+      </body>
+      </html>
+    `;
+
+    // Open a new window with the video content
+    var videoWindow = window.open();
+    videoWindow.document.write(videoContent);
+
+    // Close the document writing to make sure the content is displayed
+    videoWindow.document.close();
+  }
